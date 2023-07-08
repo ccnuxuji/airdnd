a178c78fdb2e3fcb61b2dea38f4795216e235e27
ccnuxuji <ccnuxuji.com>
2023-07-07 21:26:29 -0700

    friday update

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
@@ -24,10 +24,34 @@ function LoginFormModal() {
             });
     };
 
+    const loginDemoUser = (e) => {
+        e.preventDefault();
+        setErrors({});
+        return dispatch(sessionActions.login({
+            credential: 'DemoUser',
+            password: 'password'
+        })).then(closeModal)
+            .catch(async (res) => {
+                const data = await res.json();
+                if (data && data.errors) {
+                    setErrors(data.errors);
+                }
+            });
+    };
+
+    useEffect(() => {
+
+    }, [credential, password]);
+
     return (
-        <>
-            <div className="login-modal">
-                <div className="login-header">Log In</div>
+        <div className="login-modal">
+            <div className="login-header">
+                <h1>Log in</h1>
+            </div>
+            <div className="login-welcome-text">
+                <h3>Welcome to Airdnd</h3>
+            </div>
+            <div className="login-form">
                 <form onSubmit={handleSubmit}>
                     <div>
                         <input
@@ -50,13 +74,30 @@ function LoginFormModal() {
                     {errors.credential && (
                         <p>{errors.credential}</p>
                     )}
-                    <div>
-                        <button type="submit">Log In</button>
+                    <div className="login-submit-button-div">
+                        <button
+                            className={(credential.length < 4 || password.length < 6) ? "disabled" : ""}
+                            type="submit"
+                            disabled={credential.length < 4 || password.length < 6}
+                        >Log In</button>
                     </div>
-
                 </form>
             </div>
-        </>
+
+            <div className="orline-divider">
+                <div className="div-line-1"></div>
+                <div className="or-word">or</div>
+                <div className="div-line-2"></div>
+            </div>
+
+            <div className="login-all-items">
+                <button className="demouser" onClick={loginDemoUser}>
+                    <i className="fa-light fa-envelope"></i>
+                    <span>Continue with Demo User</span>
+                </button>
+            </div>
+
+        </div>
     );
 }
 
